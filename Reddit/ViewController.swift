7995ff9629e391e02d53d3370cf09f1dbeefc942
Andrew Tenno <andrew.tenno@gmail.com>
2019-03-09 10:41:27 -0800

    Initial Commit

---
+++ Reddit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reddit
+//
+//  Created by Andrew Tenno on 3/9/19.
+//  Copyright © 2019 Andrew Tenno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
